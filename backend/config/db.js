9137b02b6aa45911653934ab171f5036422bd730
Simon Mutunga <107920763+Issimonmutunga@users.noreply.github.com>
2025-03-29 10:50:02 +0300

    Database configuration

---
+++ backend/config/db.js
@@ -0,0 +1,25 @@
+//DATABASE CONFIG
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT
+    }
+);
+
+
+(async () => {
+    try{
+        await sequelize.authenticate();
+        console.log(`Database connected sucessfully`)
+    } catch (error) {
+        console.log(`Unable to connect to database:`, error)
+    }
+})();
+
+module.exports = sequelize;
