f9e99fb4585428b106e743fd195a5cae3417eef0
Simon Mutunga <107920763+Issimonmutunga@users.noreply.github.com>
2025-03-31 12:44:36 +0300

    Routes added

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,14 @@ const express = require('express');
 const cors = require('cors');
 const sequelize = require(`./config/db`);
 require(`./models`)//Ensures models are synced
+const db = require(`./config/db`);
 
 const app = express();
 
 //Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); //Parses json data
+app.use(`/api/auth`,require(`./routes/authRoutes`));
 
 //Test route
 app.get('/',(req,res) => {
@@ -17,4 +19,16 @@ app.get('/',(req,res) => {
 
 //Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+//app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.listen(PORT, async () => {
+    try{
+        await db.authenticate();
+        console.log('Database Connected successfully');
+        await db.sync();
+        console.log('Database synced...');    
+    } catch (error) {
+        console.error('Database connection failed:', error);    
+    }
+    console.log(`Server running on port ${PORT}`);
+});
