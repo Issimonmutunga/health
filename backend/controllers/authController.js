d7c61168cde9e8c3fac56a8ffbeec289ccd1a93a
Simon Mutunga <107920763+Issimonmutunga@users.noreply.github.com>
2025-04-01 09:01:47 +0300

    profiles

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -55,3 +55,17 @@ export const login = async (req, res) => {
         res.status(500).send('Server error');
     }
 };
+export const getProfile = async (req, res) => {
+    try {
+        const user = await User.findByPk(req.user.id, {
+            attributes: { exclude: ["password"] },
+        });
+        if (!user) return res.status(404).json({ msg: "User not found" });
+
+        res.json(user);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Server error");
+    }
+};
+
