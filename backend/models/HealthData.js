20118718bd1aac557dc6090375a0309a13ac45a3
Simon Mutunga <107920763+Issimonmutunga@users.noreply.github.com>
2025-04-02 10:13:20 +0300

    health data

---
+++ backend/models/HealthData.js
@@ -0,0 +1,12 @@
+//Tracking ----
+import { DataTypes } from 'sequelize';
+import sequelize from '../config/database.js';
+
+const HealthData = sequelize.define('HealthData', {
+  userId: { type: DataTypes.INTEGER, allowNull: false },
+  weight: { type: DataTypes.FLOAT },
+  bloodPressure: { type: DataTypes.STRING },
+  recordedAt: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
+});
+
+export default HealthData;
