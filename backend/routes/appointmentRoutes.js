0e0e2fd55ebcab5c1926fa167af320c97e63d101
Simon Mutunga <107920763+Issimonmutunga@users.noreply.github.com>
2025-04-02 10:16:06 +0300

    Cancel appointment/crud

--- backend/routes/appointmentRoutes.js
+++ backend/routes/appointmentRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { protect, authorize } from "../middleware/authMiddleware.js";
 import { createAppointment, getAppointments } from "../controllers/appointmentController.js";
+import { cancelAppointment } from "../controllers/appointmentController.js";
 
 const router = express.Router();
 
@@ -9,5 +10,8 @@ router.post("/", protect, authorize("patient"), createAppointment);
 
 // Get all appointments (Only doctors)
 router.get("/", protect, authorize("doctor"), getAppointments);
+
+//Cancel appointment --to be edited
+router.delete("/:id", cancelAppointment);
 
 export default router;
